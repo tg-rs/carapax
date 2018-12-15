e995c097ebfc41557209183bd5e84d50a000a5b9
Ross Nomann <rossnomann@protonmail.com>
2018-12-15 17:01:49 +0300

    Merge chat related types

--- src/types/chat.rs
+++ src/types/chat.rs
@@ -1,6 +1,6 @@
-use crate::types::chat_photo::ChatPhoto;
 use crate::types::message::Message;
 use crate::types::primitive::Integer;
+use crate::types::user::User;
 
 /// This object represents a chat.
 #[derive(Debug)]
@@ -31,4 +31,81 @@ pub struct Chat {
     pub sticker_set_name: Option<String>,
     /// True, if the bot can change the group sticker set. Returned only in getChat.
     pub can_set_sticker_set: Option<bool>,
+}
+
+/// This object contains information about one member of a chat.
+#[derive(Debug)]
+pub struct ChatMember {
+    /// Information about the user
+    pub user: User,
+    /// The member's status in the chat.
+    /// Can be “creator”, “administrator”, “member”, “restricted”, “left” or “kicked”
+    pub status: String, // TODO: enum
+    /// Restricted and kicked only.
+    /// Date when restrictions will be lifted for this user, unix time
+    pub until_date: Option<Integer>,
+    /// Administrators only.
+    /// True, if the bot is allowed
+    /// to edit administrator privileges of that user
+    pub can_be_edited: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can change
+    /// the chat title, photo and other settings
+    pub can_change_info: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can post in the channel, channels only
+    pub can_post_messages: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can edit messages
+    /// of other users and can pin messages, channels only
+    pub can_edit_messages: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can delete messages of other users
+    pub can_delete_messages: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can invite new users to the chat
+    pub can_invite_users: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can restrict, ban or unban chat members
+    pub can_restrict_members: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can pin messages, supergroups only
+    pub can_pin_messages: Option<bool>,
+    /// Administrators only.
+    /// True, if the administrator can
+    /// add new administrators with a subset
+    /// of his own privileges or
+    /// demote administrators that he has promoted,
+    /// directly or indirectly
+    /// (promoted by administrators that were appointed by the user)
+    pub can_promote_members: Option<bool>,
+    /// Restricted only.
+    /// True, if the user can send
+    /// text messages, contacts, locations and venues
+    pub can_send_messages: Option<bool>,
+    /// Restricted only.
+    /// True, if the user can send
+    /// audios, documents, photos, videos,
+    /// video notes and voice notes, implies can_send_messages
+    pub can_send_media_messages: Option<bool>,
+    /// Restricted only.
+    /// True, if the user can send
+    /// animations, games, stickers
+    /// and use inline bots, implies can_send_media_messages
+    pub can_send_other_messages: Option<bool>,
+    /// Restricted only.
+    /// True, if user may add web page previews
+    /// to his messages, implies can_send_media_messages
+    pub can_add_web_page_previews: Option<bool>,
+}
+
+/// This object represents a chat photo.
+#[derive(Debug)]
+pub struct ChatPhoto {
+    /// Unique file identifier of small (160x160) chat photo.
+    /// This file_id can be used only for photo download.
+    pub small_file_id: String,
+    /// Unique file identifier of big (640x640) chat photo.
+    /// This file_id can be used only for photo download.
+    pub big_file_id: String,
 }
