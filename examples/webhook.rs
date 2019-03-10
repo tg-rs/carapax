df00b3eeefd0d555742df41786169bcb87764b6d
Ross Nomann <rossnomann@protonmail.com>
2019-03-10 20:22:01 +0300

    Add new examples

---
+++ examples/webhook.rs
@@ -0,0 +1,18 @@
+use dotenv::dotenv;
+use env_logger;
+use log;
+use tgbot::{handle_updates, types::Update, UpdateHandler, UpdateMethod};
+
+struct Handler;
+
+impl UpdateHandler for Handler {
+    fn handle(&mut self, update: Update) {
+        log::info!("got an update: {:?}\n", update);
+    }
+}
+
+fn main() {
+    dotenv().ok();
+    env_logger::init();
+    handle_updates(UpdateMethod::webhook(([127, 0, 0, 1], 8080), "/"), Handler);
+}
