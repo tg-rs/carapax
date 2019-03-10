527fd3a2f46cd818b647b0a0e4d586b6846d17e3
Ross Nomann <rossnomann@protonmail.com>
2019-03-10 20:31:40 +0300

    Use api in poll example

--- examples/poll.rs
+++ examples/poll.rs
@@ -1,14 +1,32 @@
 use dotenv::dotenv;
 use env_logger;
+use futures::Future;
 use log;
 use std::env;
-use tgbot::{handle_updates, types::Update, Api, UpdateHandler, UpdateMethod};
+use tgbot::{
+    handle_updates,
+    methods::SendMessage,
+    types::{Update, UpdateKind},
+    Api, UpdateHandler, UpdateMethod,
+};
 
-struct Handler;
+struct Handler {
+    api: Api,
+}
 
 impl UpdateHandler for Handler {
     fn handle(&mut self, update: Update) {
         log::info!("got an update: {:?}\n", update);
+        if let UpdateKind::Message(message) = update.kind {
+            if let Some(text) = message.get_text() {
+                let chat_id = message.get_chat_id();
+                let method = SendMessage::new(chat_id, text.data.clone());
+                self.api.spawn(self.api.execute(&method).then(|x| {
+                    log::info!("sendMessage result: {:?}\n", x);
+                    Ok::<(), ()>(())
+                }));
+            }
+        }
     }
 }
 
@@ -23,5 +41,5 @@ fn main() {
         None => Api::new(token),
     }
     .expect("Failed to create API");
-    handle_updates(UpdateMethod::poll(api), Handler);
+    handle_updates(UpdateMethod::poll(api.clone()), Handler { api });
 }
