1aa924e0e0c915a8e41b3bacf8ffaee0c500df78
Ross Nomann <rossnomann@protonmail.com>
2019-03-18 17:40:10 +0300

    Update prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,2 +1,2 @@
 pub use crate::{app::*, context::*, dispatcher::*, handler::*};
-pub use tgbot::prelude::*;
+pub use tgbot::{methods::*, types::*, Api, ApiFuture, UpdateMethod, UpdatesStream, UpdatesStreamOptions};
