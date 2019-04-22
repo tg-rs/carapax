229cc75da9d5d843596241384007afe9d4afc789
Ross Nomann <rossnomann@protonmail.com>
2019-04-22 04:23:08 +0300

    Add tests for location type

--- tgbot/src/types/location.rs
+++ tgbot/src/types/location.rs
@@ -9,3 +9,48 @@ pub struct Location {
     /// Latitude as defined by sender
     pub latitude: Float,
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::types::{Message, MessageData, Update, UpdateKind};
+
+    #[test]
+    fn parse() {
+        let update: Update = serde_json::from_value(serde_json::json!({
+            "update_id": 10000,
+            "message": {
+                "date": 1441645532,
+                "chat": {
+                    "last_name": "Test Lastname",
+                    "type": "private",
+                    "id": 1111111,
+                    "first_name": "Test Firstname",
+                    "username": "Testusername"
+                },
+                "message_id": 1365,
+                "from": {
+                    "last_name": "Test Lastname",
+                    "id": 1111111,
+                    "first_name": "Test Firstname",
+                    "username": "Testusername",
+                    "is_bot": false
+                },
+                "location": {
+                    "longitude": 2.0,
+                    "latitude": 1.0
+                }
+            }
+        }))
+        .unwrap();
+        if let UpdateKind::Message(Message {
+            data: MessageData::Location(data),
+            ..
+        }) = update.kind
+        {
+            assert_eq!(data.latitude, 1.0);
+            assert_eq!(data.longitude, 2.0);
+        } else {
+            panic!("Unexpected update {:?}", update);
+        }
+    }
+}
