a18478a01e310eebcaac40ea46f7cf97e907cf2d
Ross Nomann <rossnomann@protonmail.com>
2019-12-23 00:53:52 +0400

    Update lib.rs in tgbot

--- tgbot/src/lib.rs
+++ tgbot/src/lib.rs
@@ -1,23 +1,25 @@
 //! A Telegram Bot API client library
 #![warn(missing_docs)]
-#![recursion_limit = "128"]
 
 mod api;
-mod executor;
 mod handler;
-mod never;
 mod request;
+
+/// Utilities to receive updates using long poll
+pub mod longpoll;
 
 /// Methods available in the Bot API
 pub mod methods;
 
-/// Types available in the Bot API
-pub mod types;
-
 /// A "prelude" for users of the library
 pub mod prelude;
 
+/// Types available in the Bot API
+pub mod types;
+
+/// Services to receive updates via webhook
+pub mod webhook;
+
 pub use self::{api::*, handler::*};
-use never::Never;
 
 pub use mime;
