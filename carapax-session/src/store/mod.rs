61cbd0c3e4d9ecd22348848e96e7d5d1471590c2
Ross Nomann <rossnomann@protonmail.com>
2019-12-31 00:59:37 +0400

    Update SessionStore trait to use async-await

--- carapax-session/src/store/mod.rs
+++ carapax-session/src/store/mod.rs
@@ -1,7 +1,7 @@
 use crate::session::SessionKey;
-use failure::Error;
-use futures::Future;
+use carapax::async_trait;
 use serde::{de::DeserializeOwned, Serialize};
+use std::error::Error;
 
 #[cfg(any(feature = "fs-store", feature = "redis-store"))]
 mod data;
@@ -19,24 +19,28 @@ pub mod fs;
 pub mod redis;
 
 /// Methods for accessing a store
+#[async_trait]
 pub trait SessionStore {
+    /// An error occurred when accessing store
+    type Error: Error + Send + Sync;
+
     /// Get value of key
     ///
     /// If key not exists, None is returned
-    fn get<O>(&self, key: SessionKey) -> Box<dyn Future<Item = Option<O>, Error = Error> + Send>
+    async fn get<O>(&mut self, key: SessionKey) -> Result<Option<O>, Self::Error>
     where
-        O: DeserializeOwned + Send + 'static;
+        O: DeserializeOwned + Send + Sync;
 
     /// Set key to hold the given value
-    fn set<I>(&self, key: SessionKey, val: &I) -> Box<dyn Future<Item = (), Error = Error> + Send>
+    async fn set<I>(&mut self, key: SessionKey, val: &I) -> Result<(), Self::Error>
     where
-        I: Serialize;
+        I: Serialize + Send + Sync;
 
     /// Set a timeout on key
     ///
     /// After the timeout has expired, the key will automatically be deleted
-    fn expire(&self, key: SessionKey, seconds: usize) -> Box<dyn Future<Item = (), Error = Error> + Send>;
+    async fn expire(&mut self, key: SessionKey, seconds: usize) -> Result<(), Self::Error>;
 
     /// Remove the specified key
-    fn del(&self, key: SessionKey) -> Box<dyn Future<Item = (), Error = Error> + Send>;
+    async fn del(&mut self, key: SessionKey) -> Result<(), Self::Error>;
 }
