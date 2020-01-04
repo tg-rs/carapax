3b576185c8284242253653b7981e25913ed4802f
Ross Nomann <rossnomann@protonmail.com>
2020-01-04 17:15:55 +0400

    Update carapax-session to use async-await and seance

---
+++ carapax-session/examples/session_counter.rs
@@ -0,0 +1,130 @@
+#[cfg(not(feature = "fs-backend"))]
+compile_error!("Enable fs-backend feature");
+
+use carapax::{
+    handler, longpoll::LongPoll, methods::SendMessage, types::Update, Api, Command, Config, Dispatcher, HandlerResult,
+};
+use carapax_session::{backend::fs::FilesystemBackend, SessionCollector, SessionManager};
+use dotenv::dotenv;
+use std::{env, time::Duration};
+use tempfile::tempdir;
+
+struct Context {
+    api: Api,
+    session_manager: SessionManager<FilesystemBackend>,
+}
+
+#[handler(command = "/set")]
+async fn handle_set(context: &mut Context, command: Command) -> HandlerResult {
+    log::info!("got a command: {:?}\n", command);
+    let message = command.get_message();
+    let chat_id = message.get_chat_id();
+    let args = command.get_args();
+    let val = if args.is_empty() {
+        0
+    } else {
+        match args[0].parse::<usize>() {
+            Ok(x) => x,
+            Err(err) => {
+                context
+                    .api
+                    .execute(SendMessage::new(chat_id, err.to_string()))
+                    .await
+                    .unwrap();
+                return HandlerResult::Stop;
+            }
+        }
+    };
+    let mut session = context.session_manager.get_session(&command);
+    session.set("counter", &val).await.unwrap();
+    context.api.execute(SendMessage::new(chat_id, "OK")).await.unwrap();
+    HandlerResult::Stop
+}
+
+#[handler(command = "/expire")]
+async fn handle_expire(context: &mut Context, command: Command) -> HandlerResult {
+    log::info!("got a command: {:?}\n", command);
+    let message = command.get_message();
+    let chat_id = message.get_chat_id();
+    let args = command.get_args();
+    let seconds = if args.is_empty() {
+        0
+    } else {
+        match args[0].parse::<u64>() {
+            Ok(x) => x,
+            Err(err) => {
+                context
+                    .api
+                    .execute(SendMessage::new(chat_id, err.to_string()))
+                    .await
+                    .unwrap();
+                return HandlerResult::Stop;
+            }
+        }
+    };
+    let mut session = context.session_manager.get_session(&command);
+    session.expire("counter", seconds).await.unwrap();
+    context.api.execute(SendMessage::new(chat_id, "OK")).await.unwrap();
+    HandlerResult::Stop
+}
+
+#[handler(command = "/reset")]
+async fn handle_reset(context: &mut Context, command: Command) -> HandlerResult {
+    log::info!("got a command: {:?}\n", command);
+    let message = command.get_message();
+    let chat_id = message.get_chat_id();
+    let mut session = context.session_manager.get_session(&command);
+    session.remove("counter").await.unwrap();
+    context.api.execute(SendMessage::new(chat_id, "OK")).await.unwrap();
+    HandlerResult::Stop
+}
+
+#[handler]
+async fn handle_update(context: &mut Context, update: Update) -> HandlerResult {
+    let message = update.get_message().unwrap();
+    log::info!("got a message: {:?}\n", message);
+    let chat_id = message.get_chat_id();
+    let mut session = context.session_manager.get_session(&update);
+    let val: Option<usize> = session.get("counter").await.unwrap();
+    let val = val.unwrap_or(0) + 1;
+    session.set("counter", &val).await.unwrap();
+    let msg = format!("Count: {}", val);
+    context.api.execute(SendMessage::new(chat_id, msg)).await.unwrap();
+    HandlerResult::Continue
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    env_logger::init();
+
+    let token = env::var("TGRS_TOKEN").expect("TGRS_TOKEN is not set");
+    let proxy = env::var("TGRS_PROXY").ok();
+
+    let mut config = Config::new(token);
+    if let Some(proxy) = proxy {
+        config = config.proxy(proxy).expect("Failed to set proxy");
+    }
+
+    let api = Api::new(config).expect("Failed to create API");
+    let tmpdir = tempdir().expect("Failed to create temp directory");
+    log::info!("Session directory: {}", tmpdir.path().display());
+
+    let backend = FilesystemBackend::new(tmpdir.path());
+
+    // spawn GC to remove old sessions
+    let gc_period = Duration::from_secs(1); // period between GC calls
+    let session_lifetime = Duration::from_secs(1); // how long session lives
+    let mut collector = SessionCollector::new(backend.clone(), gc_period, session_lifetime);
+    tokio::spawn(async move { collector.run().await });
+
+    let mut dispatcher = Dispatcher::new(Context {
+        api: api.clone(),
+        session_manager: SessionManager::new(backend),
+    });
+    dispatcher.add_handler(handle_expire);
+    dispatcher.add_handler(handle_reset);
+    dispatcher.add_handler(handle_set);
+    dispatcher.add_handler(handle_update);
+    LongPoll::new(api, dispatcher).run().await
+}
--- carapax-session/src/lib.rs
+++ carapax-session/src/lib.rs
@@ -1,15 +1,79 @@
 #![warn(missing_docs)]
 //! A session utilities for carapax
+use carapax::{
+    types::{Message, Update},
+    Command,
+};
+use seance::backend::SessionBackend;
 
-mod factory;
-mod gc;
-mod session;
-
-pub use self::{
-    factory::SessionFactory,
-    gc::{run_gc, GarbageCollector},
-    session::{Session, SessionKey, SessionLifetime},
+pub use seance::{
+    backend, Session, SessionCollector, SessionCollectorHandle, SessionError, SessionManager as BaseSessionManager,
 };
 
-/// Contains session store implementations
-pub mod store;
+/// A session manager
+#[derive(Clone)]
+pub struct SessionManager<B> {
+    inner: BaseSessionManager<B>,
+}
+
+impl<B> SessionManager<B>
+where
+    B: SessionBackend,
+{
+    /// Creates a new manager
+    ///
+    /// # Arguments
+    ///
+    /// * backend - A session store backend
+    pub fn new(backend: B) -> Self {
+        Self {
+            inner: BaseSessionManager::new(backend),
+        }
+    }
+
+    /// Returns a session for an update/message/command
+    pub fn get_session<I>(&self, input: I) -> Session<B>
+    where
+        I: Into<SessionId>,
+    {
+        self.inner.get_session(&input.into().0)
+    }
+
+    /// Returns a session by ID
+    pub fn get_session_by_id<I>(&self, id: I) -> Session<B>
+    where
+        I: AsRef<str>,
+    {
+        self.inner.get_session(id.as_ref())
+    }
+}
+
+/// Session ID obtained from Update, Message, etc...
+pub struct SessionId(String);
+
+impl From<&Update> for SessionId {
+    fn from(update: &Update) -> SessionId {
+        let (chat_id, user_id) = match (update.get_chat_id(), update.get_user().map(|x| x.id)) {
+            (Some(chat_id), Some(user_id)) => (chat_id, user_id),
+            (Some(chat_id), None) => (chat_id, chat_id),
+            (None, Some(user_id)) => (user_id, user_id),
+            (None, None) => unreachable!(), // There is always chat_id or user_id
+        };
+        SessionId(format!("{}-{}", chat_id, user_id))
+    }
+}
+
+impl From<&Message> for SessionId {
+    fn from(message: &Message) -> SessionId {
+        let (chat_id, user_id) = match (message.get_chat_id(), message.get_user().map(|x| x.id)) {
+            (chat_id, Some(user_id)) => (chat_id, user_id),
+            (chat_id, None) => (chat_id, chat_id),
+        };
+        SessionId(format!("{}-{}", chat_id, user_id))
+    }
+}
+impl From<&Command> for SessionId {
+    fn from(command: &Command) -> SessionId {
+        SessionId::from(command.get_message())
+    }
+}
