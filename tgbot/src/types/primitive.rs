9b351ff91627cca09e5154c9043f1cb4e0195c09
Ross Nomann <rossnomann@protonmail.com>
2020-01-27 05:13:52 +0400

    Add ParseMode::escape method

--- tgbot/src/types/primitive.rs
+++ tgbot/src/types/primitive.rs
@@ -1,4 +1,10 @@
 use serde::Serialize;
+use std::fmt;
+
+const ESCAPE_MARKDOWN: [char; 4] = ['_', '*', '`', '['];
+const ESCAPE_MARKDOWNV2: [char; 18] = [
+    '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!',
+];
 
 /// Telegram Integer type
 pub type Integer = i64;
@@ -21,13 +27,64 @@ pub enum ParseMode {
     MarkdownV2,
 }
 
-impl ToString for ParseMode {
-    fn to_string(&self) -> String {
-        String::from(match self {
-            ParseMode::Html => "HTML",
-            ParseMode::Markdown => "Markdown",
-            ParseMode::MarkdownV2 => "MarkdownV2",
-        })
+impl ParseMode {
+    /// Escape HTML or Markdown special characters
+    ///
+    /// For MarkdownV2 you must escape characters with `\` by yourself in the following cases:
+    ///
+    /// * all \` and `\` characters in 'pre' and 'code'.
+    /// * `(...)` - part of inline link definition, all `)` and `\` characters.
+    ///
+    /// In all other places use this method to escape special characters.
+    pub fn escape<T>(self, input: T) -> String
+    where
+        T: Into<String>,
+    {
+        let input = input.into();
+        let mut result = String::new();
+        match self {
+            ParseMode::Html => {
+                for i in input.chars() {
+                    match i {
+                        '<' => result += "&lt;",
+                        '>' => result += "&gt;",
+                        '&' => result += "&amp;",
+                        _ => result.push(i),
+                    }
+                }
+            }
+            ParseMode::Markdown => {
+                for i in input.chars() {
+                    if ESCAPE_MARKDOWN.contains(&i) {
+                        result.push('\\');
+                    }
+                    result.push(i);
+                }
+            }
+            ParseMode::MarkdownV2 => {
+                for i in input.chars() {
+                    if ESCAPE_MARKDOWNV2.contains(&i) {
+                        result.push('\\');
+                    }
+                    result.push(i);
+                }
+            }
+        }
+        result
+    }
+}
+
+impl fmt::Display for ParseMode {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            out,
+            "{}",
+            match self {
+                ParseMode::Html => "HTML",
+                ParseMode::Markdown => "Markdown",
+                ParseMode::MarkdownV2 => "MarkdownV2",
+            }
+        )
     }
 }
 
@@ -46,5 +103,15 @@ mod tests {
         assert_eq!(ParseMode::Html.to_string(), "HTML");
         assert_eq!(ParseMode::Markdown.to_string(), "Markdown");
         assert_eq!(ParseMode::MarkdownV2.to_string(), "MarkdownV2");
+    }
+
+    #[test]
+    fn parse_mode_escape() {
+        assert_eq!(ParseMode::Html.escape("<>&"), "&lt;&gt;&amp;");
+        assert_eq!(ParseMode::Markdown.escape(r#"_*`["#), r#"\_\*\`\["#);
+        assert_eq!(
+            ParseMode::MarkdownV2.escape(r#"_*[]()~`>#+-=|{}.!"#),
+            r#"\_\*\[\]\(\)\~\`\>\#\+\-\=\|\{\}\.\!"#
+        );
     }
 }
