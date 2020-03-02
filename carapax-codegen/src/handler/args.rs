5b865878bb8c2315c5d99c359980085b47a33f35
Ross Nomann <rossnomann@protonmail.com>
2020-03-02 04:20:30 +0400

    Ensure that command name starts with / in #[handler] proc macro

--- carapax-codegen/src/handler/args.rs
+++ carapax-codegen/src/handler/args.rs
@@ -1,5 +1,3 @@
-use proc_macro2::TokenStream;
-use quote::ToTokens;
 use syn::{
     parse::{Parse, ParseStream},
     Ident, LitStr, Path, Result as SynResult, Token,
@@ -10,32 +8,8 @@ pub(crate) struct HandlerArgs {
 }
 
 pub(super) enum HandlerKind {
-    Command(Value),
+    Command(LitStr),
     Predicate(Path),
-}
-
-pub(super) enum Value {
-    Str(LitStr),
-    Path(Path),
-}
-
-impl Parse for Value {
-    fn parse(input: ParseStream) -> SynResult<Self> {
-        Ok(if input.peek(LitStr) {
-            Self::Str(input.parse()?)
-        } else {
-            Self::Path(input.parse()?)
-        })
-    }
-}
-
-impl ToTokens for Value {
-    fn to_tokens(&self, tokens: &mut TokenStream) {
-        match self {
-            Self::Str(value) => value.to_tokens(tokens),
-            Self::Path(value) => value.to_tokens(tokens),
-        }
-    }
 }
 
 impl Parse for HandlerArgs {
@@ -44,7 +18,11 @@ impl Parse for HandlerArgs {
         let kind = match arg.to_string().as_ref() {
             "command" => {
                 input.parse::<Token![=]>()?;
-                HandlerKind::Command(input.parse()?)
+                let name = input.parse::<LitStr>()?;
+                if !name.value().starts_with('/') {
+                    return Err(input.error("command name must start with '/'"));
+                }
+                HandlerKind::Command(name)
             }
             "predicate" => {
                 input.parse::<Token![=]>()?;
