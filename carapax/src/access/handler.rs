b6e2e78fa90bc6876ef087d7504761dea808c450
ark0f <arsenylyashenko.3@gmail.com>
2021-03-02 16:06:58 +0300

    Add access handler test when error is occurred

--- carapax/src/access/handler.rs
+++ carapax/src/access/handler.rs
@@ -40,6 +40,14 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{
+        types::{Message, MessageData, MessageKind, PrivateChat, Update, UpdateKind, User},
+        Api, Data, Dispatcher, UpdateHandler,
+    };
+    use std::sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    };
 
     async fn policy_true() -> bool {
         true
@@ -60,5 +68,66 @@ mod tests {
             HandlerResult::Stop => { /*ok*/ }
             result => panic!("Unexpected handler result: {:?}", result),
         }
+    }
+
+    struct NotPassedData;
+
+    async fn not_passed_data(_data: Data<NotPassedData>) -> bool {
+        true
+    }
+
+    async fn execution_check(data: Data<AtomicBool>) {
+        data.store(true, Ordering::Relaxed);
+    }
+
+    #[tokio::test]
+    async fn handler_error() {
+        let flag = Arc::new(AtomicBool::new(false));
+
+        let api = Api::new("123").unwrap();
+        let mut dispatcher = Dispatcher::new(api);
+        dispatcher
+            .add_handler(not_passed_data.access())
+            .add_handler(execution_check)
+            .data_arc(flag.clone());
+
+        let update = Update {
+            id: 0,
+            kind: UpdateKind::Message(Message {
+                id: 0,
+                date: 0,
+                kind: MessageKind::Private {
+                    chat: PrivateChat {
+                        id: 0,
+                        first_name: "".to_string(),
+                        last_name: None,
+                        username: None,
+                        photo: None,
+                        bio: None,
+                        pinned_message: None,
+                    },
+                    from: User {
+                        id: 0,
+                        is_bot: false,
+                        first_name: "".to_string(),
+                        last_name: None,
+                        username: None,
+                        language_code: None,
+                    },
+                },
+                forward: None,
+                reply_to: None,
+                via_bot: None,
+                edit_date: None,
+                media_group_id: None,
+                data: MessageData::Empty,
+                reply_markup: None,
+                sender_chat: None,
+            }),
+        };
+        dispatcher.handle(update).await;
+
+        let flag = flag.load(Ordering::Relaxed);
+        assert!(!flag);
     }
 }
