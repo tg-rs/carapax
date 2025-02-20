1c649032e95066c6a81dc402fba8858a53072c16
rossnomann <rossnomann@users.noreply.github.com>
2025-02-20 23:14:17 +0000

    deploy: 46798d0c4811f9c5eed88d02e1bd077410d9ea55

---
+++ search.desc/derive_more/derive_more-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("derive_more", 0, "What <code>#[derive(From)]</code> generates\nModule containing derive definitions only, without their …\nModule containing derive definitions with their …\nWhat <code>#[derive(From)]</code> generates\nWhat <code>#[derive(From)]</code> generates")
