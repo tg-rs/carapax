096d30c258949fb6f5e43a134f0692daac14235e
rossnomann <rossnomann@users.noreply.github.com>
2025-07-05 09:08:40 +0000

    deploy: 5baaf8960a67fe3723304034f7ccd727a206d754

---
+++ trait.impl/rand/distr/uniform/trait.SampleUniform.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["governor",[["impl <a class=\"trait\" href=\"rand/distr/uniform/trait.SampleUniform.html\" title=\"trait rand::distr::uniform::SampleUniform\">SampleUniform</a> for <a class=\"struct\" href=\"governor/nanos/struct.Nanos.html\" title=\"struct governor::nanos::Nanos\">Nanos</a>"]]],["rand",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[281,12]}
---
+++ trait.impl/tgbot/handler/trait.UpdateHandler.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["carapax",[["impl&lt;H, HI, HO&gt; <a class=\"trait\" href=\"carapax/handler/trait.UpdateHandler.html\" title=\"trait carapax::handler::UpdateHandler\">UpdateHandler</a> for <a class=\"struct\" href=\"carapax/struct.App.html\" title=\"struct carapax::App\">App</a>&lt;H, HI&gt;<div class=\"where\">where\n    H: <a class=\"trait\" href=\"carapax/trait.Handler.html\" title=\"trait carapax::Handler\">Handler</a>&lt;HI, Output = HO&gt; + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> + 'static,\n    HI: <a class=\"trait\" href=\"carapax/trait.TryFromInput.html\" title=\"trait carapax::TryFromInput\">TryFromInput</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> + 'static,\n    HI::<a class=\"associatedtype\" href=\"carapax/trait.TryFromInput.html#associatedtype.Error\" title=\"type carapax::TryFromInput::Error\">Error</a>: 'static,\n    HO: <a class=\"trait\" href=\"carapax/trait.IntoHandlerResult.html\" title=\"trait carapax::IntoHandlerResult\">IntoHandlerResult</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + 'static,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1312]}
